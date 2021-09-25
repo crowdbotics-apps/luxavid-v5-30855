4ee68fb9c168445657d903b24bf420b53791a19a
Crowdbotics <team@crowdbotics.com>
2021-09-25 02:49:07 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "luxavid_v5_30855",
+  url: "https://luxavid_v5_30855.botics.co",
+  api: "https://luxavid_v5_30855.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
